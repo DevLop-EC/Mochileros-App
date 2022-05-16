9d2ffdbbaff99eecbc94958c4662367de14b99f0
DevLop-EC <christlopbsc12@gmail.com>
2022-05-16 13:52:41 -0500

    Agregar archivos de proyecto.

---
+++ Mochileros2App/Mochileros2App/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mochileros2App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+            string email = emailEntry.Text;
+            string password = passwordEntry.Text;
+            if(string.IsNullOrEmpty(email)|| password == "")
+            {
+                DisplayAlert("Advertencia", "Acceso denegado", "Ok");
+            }
+            else
+            {
+                // DisplayAlert("Exito", "Ingreso exitoso", "Ok");
+                Navigation.PushAsync(new HomePage());
+            }
+
+
+
+            
+        }
+    }
+}
