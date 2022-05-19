deabeca8ca1e03a7fc60149b095533f099373103
DevLop-EC <christlopbsc12@gmail.com>
2022-05-19 14:21:58 -0500

    Create model users for authentication

---
+++ Mochileros2App/Mochileros2App/Data/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Text;
+using Mochileros2App.Models;
+
+namespace Mochileros2App.Data
+{
+    public class DatabaseContext
+    {
+        public SQLiteAsyncConnection Connection { get; set; }
+
+        public DatabaseContext(string path)
+        {
+            Connection = new SQLiteAsyncConnection(path);
+            Connection.CreateTableAsync<Users>().Wait();
+
+        }
+    }
+}
---
+++ Mochileros2App/Mochileros2App/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mochileros2App.Models
+{
+    public class Users
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Status { get; set; }
+    }
+}
