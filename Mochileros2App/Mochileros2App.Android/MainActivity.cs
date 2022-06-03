eb3fc8a08b88d294fcff92d936066dd9ed3a52e0
DevLop-EC <christlopbsc12@gmail.com>
2022-06-03 16:22:21 -0500

    Simple splash screen animation

--- Mochileros2App/Mochileros2App.Android/MainActivity.cs
+++ Mochileros2App/Mochileros2App.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Mochileros2App.Droid
 {
-    [Activity(Label = "Mochileros2App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Mochileros2App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
